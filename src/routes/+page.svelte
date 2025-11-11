a453d7c5e7dc7f933a9360d3a8ace27f66cb7736
Jolvera2001 <jolvera6@stedwards.edu>
2025-11-10 23:27:49 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { markedHighlight } from "marked-highlight";
+  import { invoke } from "@tauri-apps/api/core";
+  import hljs from "highlight.js";
+  import { Marked } from "marked";
+
+  import 'highlight.js/styles/github-dark.css';
+
+  const marked = new Marked(
+    markedHighlight({
+      emptyLangClass: 'hljs',
+      langPrefix: 'hljs language-',
+      highlight(code, lang, info) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+      }
+    })
+  );
+
+  let content = $state("");
+  let markdown = $derived(marked.parse(content));
+
+  // async function greet(event: Event) {
+  //   event.preventDefault();
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   greetMsg = await invoke("greet", { name });
+  // }
+</script>
+
+<main class="container">
+  <textarea bind:value={content}></textarea>
+  <div>{@html markdown}</div>
+</main>
