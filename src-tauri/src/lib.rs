8886cd353695d908cebad90293f28dccc25cb380
Jolvera2001 <jolvera6@stedwards.edu>
2025-11-13 23:05:56 -0600

    cleaned up comnmand

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,19 @@
-mod document;
 mod app_error;
+mod document;
 
 use tauri::State;
 
-use crate::document::{Document, DocumentState};
-use crate::app_error::AppError;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use crate::{
+    app_error::AppError,
+    document::{Document, DocumentState},
+};
 
 #[tauri::command]
-async fn load_files(directory_path: String, documents: State<'_, DocumentState>) -> Result<Vec<Document>, String> {
-    documents.fetch_test_files(&directory_path).await; // TODO: add .map_err for proper error handling
+async fn load_files(
+    directory_path: &str,
+    documents: State<'_, DocumentState>,
+) -> Result<Vec<Document>, AppError> {
+    documents.fetch_test_files(directory_path).await?;
     Ok(documents.get_documents())
 }
 
@@ -24,7 +23,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .manage(DocumentState::new())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, load_files])
+        .invoke_handler(tauri::generate_handler![load_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
